684e469a390f44581ce24ab9dc3d52cbd7936d6c
Yue Wang <hin@Yue-Wang.local>
2018-12-09 21:12:22 +0000

    modify home page test

--- cypress/integration/home-page.spec.js
+++ cypress/integration/home-page.spec.js
@@ -5,11 +5,15 @@ describe('Home page', () => {
 
   it('Shows a header', () => {
     cy.get('.vue-title').should('contain', 'Homer for Customer !!');
+    cy.get('b > i').should('contain', 'Time for a reservation !!')
+    cy.get('tr > :nth-child(1) > :nth-child(2)')
+    cy.get('tr > :nth-child(1) > :nth-child(2) > a').should('have.attr', 'href', '#/room')
+    cy.get('tr > :nth-child(1) > :nth-child(3)').should('contain', 'the Room page and find your room')
+    cy.get('.hero > :nth-child(4)').should('contain', 'This is the homepage of hotelBooking Web app')
   });
 
   describe('Navigation bar', () => {
     it('Shows the required links', () => {
-      // cy.get('.navbar').find('[data-test=bookbtn]')
       cy.get('.navbar').contains('Home')
       cy.get('.navbar-nav:nth-child(1)').within(() => {
         cy.get('.nav-item:first').should('contain', 'Home')
@@ -28,11 +32,12 @@ describe('Home page', () => {
       // cy.get('[data-test=bookbtn]').click();
       // cy.get('.navbar').contains('Book').click();
       // cy.url().should('include', '/book')
-      cy.get('.navbar').contains('Room').click();
+      cy.get('.navbar').contains('Room').click()
       cy.url().should('include', '/room')
-      cy.get('.navbar').contains('Manage').click();
+      cy.get('.navbar').contains('Manage').click()
       cy.url().should('include', '/bookings')
-      // etc
+      cy.get(':nth-child(4) > .nav-link > .fa').click()
+      cy.url().should('include', '/book')
     });
   })
 });
